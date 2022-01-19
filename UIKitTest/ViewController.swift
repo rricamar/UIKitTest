a31873f5e2aa75a728d6c1653b083232c73adaa2
rricamar <r.ricamarti@gmail.com>
2022-01-19 11:17:30 +0100

    feat(main.storyboard): add button and switch components

--- UIKitTest/ViewController.swift
+++ UIKitTest/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount: Int = 0;
+    
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var switchLabel: UILabel!
+    
+    @IBOutlet weak var switchComponent: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setSwitchText()
     }
 
-
+    @IBAction func buttonTouchUpInside(_ sender: Any) {
+        buttonCount += 1;
+        countLabel.text = "Click count: \(buttonCount)";
+    }
+    
+    @IBAction func switchValueChanged(_ sender: Any) {
+        setSwitchText()
+    }
+    
+    func setSwitchText() {
+        if switchComponent.isOn {
+            switchLabel.text = "Switch value: true";
+        } else {
+            switchLabel.text = "Switch value: false";
+        }
+    }
 }
 
